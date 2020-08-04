e34ba9eeb8ee8bc4b5b0d3372fc669a377967b9f
Karunakar Gadireddy <karunakar.gadireddy@V20530002noMacBook-puro.local>
2020-08-04 17:40:34 +0530

    Added run script (WPI)

--- scripts/run.py
+++ scripts/run.py
@@ -3,10 +3,15 @@
 """
 
 from autotrade.misc.datatypes import Ticker
-from autotrade.market_api.nyse import NSE_MARKET_API
+from autotrade.market_api.nyse import NYSE_MARKET_API
 
+listing_path = "./data/nasdaq.txt"
 
-market_api = NYSE_MARKET_API()
+market_api = NYSE_MARKET_API(listing_path, "|")
+
+all_tickers = market_api.get_all_stocks()
 
 a = Ticker("TTM", "Tata Motors")
-market_api.get_historic_quote(a)
+historic_data = market_api.get_historic_quote(a)
+print(market_api.get_quote(a))
+print(len(market_api.get_all_stocks()))
