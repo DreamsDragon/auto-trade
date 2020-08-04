239b7248acf74de8028e1fd85b179f866fd3e351
Karunakar Gadireddy <karunakar.gadireddy@V20530002noMacBook-puro.local>
2020-08-04 17:12:27 +0530

    added functions to get historic and global quote

--- autotrade/market_api/nyse.py
+++ autotrade/market_api/nyse.py
@@ -3,37 +3,45 @@
 """
 import os
 import urllib.request
-
+from json import loads
+from datetime import datetime
 
 from . import AbstractMarketAPI
-from ..misc.datatypes import Ticker, Time
+from ..misc.datatypes import Ticker, Time, Quote, Price
 
 
-def probe_nyse(ticker_symbol: str, interval=1):
-    key = os.environ["ALPHA_VANTAGE_KEY"]
-    if interval > 60:
-        interval = 60
-    if interval < 1:
-        interval = 1
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={0}&interval={1}min&apikey={2}".format(
-        ticker_symbol, interval, key
-    )
+def probe_nyse(url: str):
+
     req = urllib.request.Request(url, headers={"User-Agent": "Chrome Browser"})
     fp = urllib.request.urlopen(req, timeout=100)
     mybytes = fp.read()
-
-    return mystr
+    data = loads(mybytes)
+    return data
 
 
 class NYSE_MARKET_API(AbstractMarketAPI):
     def __init__(self):
-        pass
+        self.key = os.environ["ALPHA_VANTAGE_KEY"]
 
     def get_quote(self, ticker: Ticker):
         """
             Get the live quote of a ticker
         """
-        pass
+        url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={0}&outputsize=compact&apikey={1}".format(
+            ticker.symbol, self.key
+        )
+        data = probe_nyse(url)["Global Quote"]
+        quote = Quote(
+            ticker,
+            datetime.now(),
+            Price(data["02. open"]),
+            Price(data["03. high"]),
+            Price(data["04. low"]),
+            -1,
+            Price(data["05. price"]),
+            Price(data["06. volume"]),
+        )
+        return quote
 
     def get_historic_quote(
         self, ticker: Ticker, start: Time = None, end: Time = None, interval: int = 1
@@ -41,10 +49,36 @@ class NYSE_MARKET_API(AbstractMarketAPI):
         """
             Get historic quotes in the time period
         """
-        print(probe_nse(ticker.symbol, interval))
+        interval = self._adjust_interval(interval)
+        url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={0}&interval={1}min&apikey={2}".format(
+            ticker.symbol, interval, self.key
+        )
+        data = probe_nyse(url)["Time Series ({0}min)".format(interval)]
+        historic_quotes = []
+        for time_stamp, values in data.items():
+            historic_quotes.append(
+                Quote(
+                    ticker,
+                    datetime.strptime(time_stamp, "%Y-%m-%d %H:%M:%S"),
+                    Price(values["1. open"]),
+                    Price(values["2. high"]),
+                    Price(values["3. low"]),
+                    Price(values["4. close"]),
+                    -1,
+                    Price(values["5. volume"]),
+                )
+            )
+        return historic_quotes
 
     def get_all_stocks(self):
         """
             Return list of all available stocks
         """
         pass
+
+    def _adjust_interval(self, interval, minimum=1, maximum=60):
+        if interval > maximum:
+            interval = maximum
+        if interval < minimum:
+            interval = minimum
+        return interval
